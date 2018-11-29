e72c08e30b1fffe862c13d024746e2366dee3ee7
Anandha Khaou <anandhakhaou@gmail.com>
2018-11-29 23:22:02 +0000

    added the controllers, views, and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   get 'doses/create'
+#   get 'doses/destroy'
+#   get 'cocktails/index'
+#   get 'cocktails/show'
+#   get 'cocktails/new'
+#   get 'cocktails/create'
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   resources :cocktails, only: [:index,:show, :new, :create] do
+#     resources :doses, only: [:new, :create, :destroy]
+#   end
+#   resources :doses, only: [:destroy]
+#   # root 'cocktails#index'
+# end
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    # resources :reviews, only: [:create]
+  end
+  resources :doses, only: [:destroy]
 end
